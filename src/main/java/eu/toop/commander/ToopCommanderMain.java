233472ad29729b426e81bcc2d9fff46080393ad0
Muhammet YILDIZ <myildiz83@gmail.com>
2019-07-21 02:52:37 +0300

    added shortcut for send-dc-request

--- src/main/java/eu/toop/commander/ToopCommanderMain.java
+++ src/main/java/eu/toop/commander/ToopCommanderMain.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2018-2019 toop.eu
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,10 @@
  */
 package eu.toop.commander;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.eclipse.jetty.server.Server;
@@ -92,6 +96,11 @@ public class ToopCommanderMain {
               CommandProcessor.printHelpMessage();
               break;
 
+            case "sdr":
+              prepareShortcutForDcRequest(command);
+              CommandProcessor.processDCDPCommand(command);
+              break;
+
             case "cat": {
               List<String> fileNames = command.getEmptyParameters();
               fileNames.forEach(fileName -> {
@@ -134,6 +143,25 @@ public class ToopCommanderMain {
       }
     } catch (UserInterruptException ex) {
       CommandProcessor.quit(server);
+    }
+  }
+
+  public static void prepareShortcutForDcRequest(Command command) {
+
+    try {
+      Field field = command.getClass().getDeclaredField("options");
+      field.setAccessible(true);
+      field.set(command, new HashMap<>());
+
+
+      field = command.getClass().getDeclaredField("mainCommand");
+      field.setAccessible(true);
+      field.set(command, "send-dc-request");
+
+      command.getOptions().put("", new ArrayList<>());
+      command.getOptions().put("f", Arrays.asList("data/request/TOOPRequest.xml"));
+    } catch (NoSuchFieldException nfe) {
+    } catch (IllegalAccessException e) {
     }
   }
 }
